0bc641341f1fee761bfb7cbe94512c01fa01e15f
Ramyakrishnapitaka <154577647+Ramyakrishnapitaka@users.noreply.github.com>
2025-10-16 12:07:27 +0530

    Update login API endpoint URL

--- testimonials_frontend/src/features/Login.jsx
+++ testimonials_frontend/src/features/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
     }),
     onSubmit: async (values) => {
       try {
-        const response = await fetch("https://testimonial-reviews-backend.onrender.com/login", {
+        const response = await fetch("https://testimonial-reviews-1.onrender.com/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(values),
